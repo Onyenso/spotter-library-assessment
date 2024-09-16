9d18562bd170c00df3d9deb4cf08abf7d713f982
Uchenna J. Onyenso <alphadev.onyenso@gmail.com>
2024-09-16 14:00:46 +0100

    clean up

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import UserManager
 
 
-# This custom manager is because I'd like the email to be used as
-# the username anytime a user is created.
 class CustomUserManager(UserManager):
     def create_superuser(self,  email, username=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
